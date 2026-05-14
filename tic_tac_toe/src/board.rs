7fc7e0c7ca8eb4802122e577457da64e84e7ce65
CenturionTheMan <dziedziakmichal0@gmail.com>
2026-05-14 13:59:09 +0200

    board impl

--- tic_tac_toe/src/board.rs
+++ tic_tac_toe/src/board.rs
@@ -1,13 +1,111 @@
-pub struct Board {
-    const GRID_SIZE: i32 = 3;
-    let matrix: [[i32;GRID_SIZE]; GRID_SIZE] = [[0;GRID_SIZE] ; GRID_SIZE];
+use std::fmt::{self};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BCell {
+    E, //empty
+    X,
+    O
 }
 
-impl Board {
+impl fmt::Display for BCell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BCell::E => write!(f, "{}", " "),
+            BCell::X => write!(f, "X"),
+            BCell::O => write!(f, "{}", "O"),
+        }
+    }
+}
 
-    fn get_position(&self, x: i32, y: i32){
-        assert!(x >= self.GRID_SIZE || x < 0 || y >= self.GRID_SIZE || y < 0, "Position out of bounds");
-        return self.matrix[x][y]
+
+#[derive(Debug)]
+pub struct Board 
+{
+    matrix: [[BCell; Self::GRID_SIZE]; Self::GRID_SIZE]
+}
+
+impl Board 
+{
+    const GRID_SIZE: usize = 3;
+
+    fn check_bounds(&self, x: usize, y: usize) -> bool
+    {
+        x < Self::GRID_SIZE && y < Self::GRID_SIZE
     }
 
+    pub fn new() -> Self 
+    {
+        Self { matrix: [[BCell::E; Self::GRID_SIZE]; Self::GRID_SIZE] }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> Option<BCell>
+    {
+        if self.check_bounds(x, y)
+        {
+            Some(self.matrix[x][y])
+        }
+        else 
+        {
+            None
+        }
+    }
+
+    pub fn set(&mut self, x: usize, y: usize, to_put: BCell) -> bool
+    {
+        match self.get(x, y)
+        {
+            Some(val) => {
+                if val == BCell::E
+                {
+                    self.matrix[x][y] = to_put;
+                    return true
+                }
+                else {
+                    return  false
+                }
+            }
+            None => panic!("Coordinates out of bounds: x={} y={}", x, y)
+        }
+
+        
+    }
+
+}
+
+impl Default for Board {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for x in 0..Self::GRID_SIZE
+        {
+            for y in 0..Self::GRID_SIZE
+            {
+                match self.get(x, y)
+                {
+                    Some(v) => {
+                        if y == Self::GRID_SIZE - 1
+                        {
+                            write!(f, "{:}", v)?
+                        }
+                        else {
+                            write!(f, "{:} | ", v)?
+                        }
+                    },
+                    None => write!(f, "X")? 
+                }
+            }
+            writeln!(f)?;
+            if x < Self::GRID_SIZE - 1
+            {
+                write!(f, "---------")?;
+                writeln!(f)?;
+            }
+        }
+
+        Ok(())
+    }
 }
--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -1,5 +1,10 @@
 mod board;
 
+use board::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = Board::default();
+    board.set(1, 1, BCell::X);
+    board.set(0, 0, BCell::O);
+    println!("{}", board)
 }
